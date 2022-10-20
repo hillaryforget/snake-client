b041fd16602b6175681b2ed9bdf7b0fb5abf1362
Hillary Forget <hillaryforget@outlook.com>
2022-10-20 10:20:47 -0400

    added txt

--- client.js
+++ client.js
@@ -10,6 +10,12 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  //sends msg to player to notify them of connection
+  conn.on('connect', () =>
+    console.log("Successfully connected to game server"));
+  conn.write("Name: HAF");
+
+
   conn.on('data', (data) => {
     console.log(data);
   });
