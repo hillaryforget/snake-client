0f4227c313259c399aebf85d5c7b2d246dc04311
Hillary Forget <hillaryforget@outlook.com>
2022-10-20 11:47:48 -0400

    added setInput, hadleUserInput

--- play.js
+++ play.js
@@ -1,5 +1,17 @@
 const net = require("net");
 const {connect} = require("./client");
 
-console.log("Connecting ...");
-connect();
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+//logs user input
+module.exports = {
+  setupInput
+};
