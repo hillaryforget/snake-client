baf90903eb56816f6a07d6a171f396ce16e6c546
Hillary Forget <hillaryforget@outlook.com>
2022-10-20 20:41:54 -0400

    added definition for conn in handleUserInput fn

--- input.js
+++ input.js
@@ -1,20 +1,37 @@
-//exit with ctrl + C
-const handleUserInput = function(data) {
-  console.log({data});
-  if (data === '\x03') {
-    console.log("bye bye");
-    process.exit();
-  }
-};
+let conn;
 
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(connection) {
+  conn = connection;
   const stdin = process.stdin;
   stdin.on("data", handleUserInput);
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   return stdin;
+};
+
+const moveUp = 'w';
+const moveDown = 's';
+const moveLeft = 'a';
+const moveRight = 'd';
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') { //'\x03' also works
+    process.exit();
+  } // exit with ctrl + c
+  if (key === moveUp) {
+    conn.write('Move: up');
+  } // W
+  if (key === moveDown) {
+    conn.write('Move: down');
+  } // S
+  if (key === moveLeft) {
+    conn.write('Move: left');
+  } // A
+  if (key === moveRight) {
+    conn.write('Move: right');
+  } // D
 };
 
 //logs user input
--- play.js
+++ play.js
@@ -2,6 +2,6 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+//prepare game to recieve user input, and write to the server
+setupInput(connect());
